4bd9cec6f4e16c62bf2beed96ce829610bd75efa
ZeelBoda <zeelboda2106@gmail.com>
2024-03-11 00:24:47 +0530

    dependecy

---
+++ backend/src/main/java/com/technicalinterview/instagramclone/Entity/Users.java
@@ -0,0 +1,62 @@
+//package com.technicalinterview.instagramclone.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.Id;
+//
+//import javax.validation.constraints.NotNull;
+//
+//
+//public class Users
+//{
+//    @Id
+//    @GeneratedValue
+//    private int id;
+//   // @NotNull
+//    @NotNull
+//    private String userId;
+//    private String username;
+//    private String name;
+//    private String profileImage;
+//    public Users() {
+//        super();
+//        // TODO Auto-generated constructor stub
+//    }
+//    public Users(int id, @NotNull String userId, String username, String name, String profileImage) {
+//        super();
+//        this.id = id;
+//        this.userId = userId;
+//        this.username = username;
+//        this.name = name;
+//        this.profileImage = profileImage;
+//    }
+//    public int getId() {
+//        return id;
+//    }
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//    public String getUserId() {
+//        return userId;
+//    }
+//    public void setUserId(String userId) {
+//        this.userId = userId;
+//    }
+//    public String getUsername() {
+//        return username;
+//    }
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//    public String getName() {
+//        return name;
+//    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//    public String getProfileImage() {
+//        return profileImage;
+//    }
+//    public void setProfileImage(String profileImage) {
+//        this.profileImage = profileImage;
+//    }
+//
+//}
---
+++ backend/src/main/java/com/technicalinterview/instagramclone/controller/Usercontroller.java
@@ -0,0 +1,18 @@
+//package com.technicalinterview.instagramclone.controller;
+//import com.technicalinterview.instagramclone.Entity.Users;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//
+//public class Usercontroller {
+//    @PostMapping("")
+//    private boolean submitUser(@RequestBody Users user) {
+//        return true;
+//    }
+//    @GetMapping("/{userid}")
+//    private Users getUserDetails(@PathVariable("userid") String userId)
+//    {
+//        return new Users();
+//    }
+//}
